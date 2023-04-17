ca05ac9848ec954852ed54382e3fab5940690d4d
Jyothsna <jyothsnamandlipalli@gmail.com>
2023-04-17 22:13:55 +0300

    roboshop.conf files

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,8 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 yum install mongodb-org -y
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
 ## Edit the place and replace 127.0.0.1 with 0.0.0.0
 
